0c92dc448289a904befec062904d5ca70e5c4018
jadoo7738-rgb <jadoo7738@gmail.com>
2026-01-15 13:34:44 +0530

    Update waitlistPanel.js

--- bot/waitlistPanel.js
+++ bot/waitlistPanel.js
@@ -1,27 +1,22 @@
 module.exports = async (channel) => {
   await channel.send({
-    embeds: [
-      {
-        title: "📝 Evaluation Testing Waitlist",
-        description:
-          "**Click the button below to enter the waitlist**\n\n" +
-          "• IGN\n• Region\n• Preferred Server\n\n" +
-          "⚠️ Fake info = instant deny",
-        color: 0x5865f2
-      }
-    ],
-    components: [
-      {
-        type: 1,
-        components: [
-          {
-            type: 2,
-            style: 1,
-            label: "Join Waitlist",
-            custom_id: "waitlist_join"
-          }
-        ]
-      }
-    ]
+    embeds: [{
+      title: "📝 Evaluation Testing Waitlist",
+      description:
+        "**Upon applying you will added to waitlist channel**\n\n" +
+        "you will get pinged when on your turn"
+        "• IGN\n• Region\n• Preferred Server\n\n" +
+        "⚠️ Fake info = instant deny",
+      color: 0x5865f2
+    }],
+    components: [{
+      type: 1,
+      components: [{
+        type: 2,
+        style: 1,
+        label: "Join Waitlist",
+        custom_id: "waitlist_join"
+      }]
+    }]
   });
 };
