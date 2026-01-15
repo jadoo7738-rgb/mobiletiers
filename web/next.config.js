4841227b17ccff52a0e53906b7400bd3d254dabc
jadoo7738-rgb <jadoo7738@gmail.com>
2026-01-15 23:42:32 +0530

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["mc-heads.net"],
-  },
+  swcMinify: true
 };
 
 module.exports = nextConfig;
